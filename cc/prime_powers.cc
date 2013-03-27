a5151c9f7b85172a271df3da0718087b447674c8
Maxwell Sayles <maxwellsayles@gmail.com>
2013-03-26 23:26:18 -0700

    one procedure for prime powers and new dat file

--- cc/prime_powers.cc
+++ cc/prime_powers.cc
@@ -16,38 +16,32 @@ const int targets[] = {1, 9, 25, 27, 49, 81, 121, 125};
 
 // Computes the probability that the order has no prime power
 // factors greater than a set of target prime power bounds.
-void probWithPower(const int i) {
-  string filename = bigIdealFilename(i);
-  cout << "Processing file \"" << filename << "\"" << endl;
-  cout << fixed << setprecision(5);
-
-  // Load ideals
-  list<Ideal> ideals;
-  loadIdeals(filename, ideals);
-
-  ofstream out;
-  out.open("prime-powers.dat", ios_base::out | ios_base::app);
-  out << fixed << setprecision(5);
-  out << i;
-
-  // Compute ratio for each target
-  for (int target : targets) {
-    double ratio = validIdealFactorsRatio<double>(ideals.begin(),
-						  ideals.end(),
-						  target);
-    cout << "Ratio after culling for target " << target
-	 << ": " << ratio << endl;
-    out << ", " << ratio;
-  }
-
-  cout << endl;
-  out << endl;
-  out.close();
-}
-
 void runPrimePowers() {
+  ofstream out("prime-powers.dat");
   for (int i = 32; i <= 80; i += 8) {
-    probWithPower(i);
+    string filename = bigIdealFilename(i);
+    cout << "Processing file \"" << filename << "\"" << endl;
+    cout << fixed << setprecision(8);
+
+    // Load ideals
+    list<Ideal> ideals;
+    loadIdeals(filename, ideals);
+
+    out << fixed << setprecision(8);
+    out << i;
+
+    // Compute ratio for each target
+    for (int target : targets) {
+      double ratio = validIdealFactorsRatio<double>(ideals.begin(),
+						    ideals.end(),
+						    target);
+      cout << "Ratio after culling for target " << target
+	   << ": " << ratio << endl;
+      out << ", " << ratio;
+    }
+
+    cout << endl;
+    out << endl;
   }
 }
 
